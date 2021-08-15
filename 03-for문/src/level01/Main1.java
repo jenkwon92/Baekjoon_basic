fd33cbbe138a5b37045edf565bd046e1b5ae26aa
sooin1306 <sottotndls@gmail.com>
2021-08-15 10:38:24 +0900

    Update Main1.java
    210815

--- 03-for문/src/level01/Main1.java
+++ 03-for문/src/level01/Main1.java
@@ -1,9 +1,18 @@
 package level01;
 
+import java.util.Scanner;
+
 public class Main1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		
+		for(int i = 1; i<10;i++) {
+			System.out.println(N+" * "+i+" = "+N*i);
+		}
+		
 
 	}
 

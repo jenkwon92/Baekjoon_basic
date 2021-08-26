35d69c318bbee6308b027e9c129a16712c25e867
Jen Kwon <70299766+jenkwon92@users.noreply.github.com>
2021-08-27 02:00:47 +0900

    210827

---
+++ 01-입출력과 사칙연산/src/level05_1000/Main.java
@@ -0,0 +1,13 @@
+package level05_1000;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int A, B;
+		A = sc.nextInt();
+		B = sc.nextInt();
+		System.out.println(A+B);
+	}
+}
